3711470ce9800217f6fb21f9f1b0304de8815fb1
Hector Chow <s20211002@carmelss.edu.hk>
2026-09-18 13:51:32 +0800

    some changes

--- battleship-serverside.py
+++ battleship-serverside.py
@@ -1,3 +1,7 @@
 import socket
-hostname = socket.gethostname()
-print(hostname)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip, port= s.getsockname()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((ip, port))
+server_socket.listen(5)
